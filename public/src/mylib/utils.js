3fc9911d66a0e7efd554d84850f5e9c0ebaccdb2
shahid1170 <96632432+shahid1170@users.noreply.github.com>
2024-04-11 00:36:38 +0500

    fornm work

--- public/src/mylib/utils.js
+++
@@ -1,6 +0,0 @@
-export function getResult(marks){
-  let pass= marks.filter(m => m >=50).sort((a,b)=>a-b);
-  let fail= marks.filter(m => m <=49).sort((a,b)=>a-b);
-  return{pass, fail};
-
-}
--- src/App.jsx
+++ src/App.jsx
@@ -3,60 +3,25 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 // import './App.css'
 // import "./mystyle.css"
-import questions from "./mylib/data.json"
+
 
 
 
 export default function App() {
-
-const [allQues, setAllQues]= useState(questions);
-
-const onSelect=(qid, op)=>{
-  console.log(qid, op)
-  const copyQues=[...allQues];
-  const ques=copyQues.find(q=>q.id===qid)
-  ques.selectedOption=op;npm
-  setAllQues(copyQues);
-  console.log(allQues);
-}
-  
-const getStyle=(q,op)=>{
-  const style="list-group-item";
-  if(op !==q.selectedOption)
-  return style;
-
-    if(op===q.answer)
-    
-    return style + "bg-success"
-  else
-    return style + "bg-danger"
-    
-  
-  
+  let [user, setUser]=useState("");
+  const onChangeUser=(event)=>{
+    const user =event.target.value;
+    setUser(user);
   }
+ 
+  return( 
+  <>
+  <input type="text" value={user} onChange={onChangeUser}/>;
+<h3>Welcome {user}</h3>
+
+</>
   
 
-
-  return (
-
-    <div className='container my-3'>
-      {allQues.map((q) => (
-        <div className="card my-2" key={q.id}>
-          <div className="card-header">{q.statement}</div>
-          <ul className="list-group list-group-flush">
-            {q.options.map((op) =>
-              <li
-                key={op}
-                className={getStyle()} onClick={() =>onSelect(q.id, op)}>{op}</li>
-            )}
-
-            
-          </ul>
-        </div>
-      ))}
-
-
-    </div>
   )
 }
 
