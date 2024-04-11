12256310d0c392dbffc92dd9f9037c9e7aa584cf
shahid1170 <96632432+shahid1170@users.noreply.github.com>
2024-04-11 22:33:31 +0500

    form

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-// import './App.css'
-// import "./mystyle.css"
+import React, { useState } from 'react';
+import { Form } from 'react-bootstrap';
 
+function FormTextExample() {
+   const [email, setEmail]=useState('');
+   const [passm setPass]=useState("");
 
-
-
-export default function App() {
-  let [user, setUser] = useState({
-    name: "",
-    age: "",
-    education:""
-  })
-
-  const onChangeUser = ({target:{name, value}}) => {
-    setUser({...user, [name]:value})
-    }
-
+   const handleEmail=(event)=> {
+    setEmail(event.target.value);
+   }
+   const handlePass=(event)=> {
+    setPass(event.target. value);
+   }
   return (
-    <div className="container m-5">
-      <h4> user Name :</h4>
-      <input type="text" name="name" value={user.name} onChange={onChangeUser}/>
-
-      <h4> user Age :</h4>
-      <input type="number" name="age" value={user.age} onChange={onChangeUser}/>
-
-      <h4> user Education :</h4>
-      <input name="education" value={user.education} onChange={onChangeUser}/>
-
-      {user.name && user.age && user.education && (
-        <h3> Dear, {user.name} you are {user.age} years old and education is {user.education}. </h3>
-      )}
-    </div>
-
+    <>      
+    <Form>
+      <Form.Group className="mb-3" controlId="formGroupEmail">
+        <Form.Label>Email address</Form.Label>
+        <Form.Control type="email" placeholder="Enter email" />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formGroupPassword">
+        <Form.Label>Password</Form.Label>
+        <Form.Control type="password" placeholder="Password" />
+      </Form.Group>
+    </Form>  
+    </>
   );
 }
 
-
-
-
-
-
-
-
-
-
-
+export default FormTextExample;
