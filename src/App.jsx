f41d3427a0331f6804bc5916ad345e4aed42bb45
shahid1170 <96632432+shahid1170@users.noreply.github.com>
2024-04-11 14:59:57 +0500

    input done

--- src/App.jsx
+++ src/App.jsx
@@ -8,22 +8,38 @@ import viteLogo from '/vite.svg'
 
 
 export default function App() {
-  let [user, setUser]=useState("");
-  const onChangeUser=(event)=>{
-    const user =event.target.value;
-    setUser(user);
-  }
- 
-  return( 
-  <>
-  <input type="text" value={user} onChange={onChangeUser}/>;
-<h3>Welcome {user}</h3>
+  let [user, setUser] = useState({
+    name: "",
+    age: "",
+    education:""
+  })
 
-</>
-  
+  const onChangeUser = ({target:{name, value}}) => {
+    setUser({...user, [name]:value})
+    }
 
-  )
+  return (
+    <div className="container m-5">
+      <h4> user Name :</h4>
+      <input type="text" name="name" value={user.name} onChange={onChangeUser}/>
+
+      <h4> user Age :</h4>
+      <input type="number" name="age" value={user.age} onChange={onChangeUser}/>
+
+      <h4> user Education :</h4>
+      <input name="education" value={user.education} onChange={onChangeUser}/>
+
+      {user.name && user.age && user.education && (
+        <h3> Dear, {user.name} you are {user.age} years old and education is {user.education}. </h3>
+      )}
+    </div>
+
+  );
 }
+
+
+
+
 
 
 
