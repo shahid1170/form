f8aa57df3a8ee28fc1573f5981f3f03194065c05
shahid1170 <96632432+shahid1170@users.noreply.github.com>
2024-04-14 22:47:27 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,19 @@
-import React, { useState } from 'react';
-import { Form } from 'react-bootstrap';
+import Form from 'react-bootstrap/Form';
+const 
 
-function FormTextExample() {
-   const [email, setEmail]=useState('');
-   const [passm setPass]=useState("");
-
-   const handleEmail=(event)=> {
-    setEmail(event.target.value);
-   }
-   const handlePass=(event)=> {
-    setPass(event.target. value);
-   }
+function FormGroupExample() {
   return (
-    <>      
     <Form>
       <Form.Group className="mb-3" controlId="formGroupEmail">
         <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" />
+        <Form.Control type="email" placeholder="Email is expected" />
       </Form.Group>
       <Form.Group className="mb-3" controlId="formGroupPassword">
         <Form.Label>Password</Form.Label>
         <Form.Control type="password" placeholder="Password" />
       </Form.Group>
-    </Form>  
-    </>
+    </Form>
   );
 }
 
-export default FormTextExample;
+export default FormGroupExample;
